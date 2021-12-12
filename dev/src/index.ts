857e0fde668e090ab003124ad4718689affda937
aron-tw <aron886tw@gmail.com>
2021-12-12 21:42:29 +0800

    env: dev

--- dev/src/index.ts
+++ dev/src/index.ts
@@ -1,5 +1,23 @@
-import '../../src/index';
+import { TextTemplate } from '../../src/index';
+import marked from 'marked';
 
-import readme from 'raw-loader!./readme.md';
+import readmeText from 'raw-loader!./readme.md';
+import masterConfig from '../../master.json';
+import packageConfig from '../../src/package.json';
 
-console.log(readme);
+const data = {
+    ...masterConfig,
+    package: packageConfig
+}
+
+console.log('data:', data);
+
+const slotTemplate = new TextTemplate(readmeText, {
+    behavior: 'slot',
+    language: 'readme'
+});
+const template = new TextTemplate(slotTemplate.render(data));
+const renderedText = template.render(data);
+
+document.getElementById('rendered-readme')
+    .innerHTML = marked.parse(renderedText);
